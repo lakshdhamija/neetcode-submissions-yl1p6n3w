bc3f9197e00a128bb9c0984f5bddb2ed48bdfc14
Laksh Dhamija <laksh.dhamija@gmail.com>
2026-08-29 16:46:03 -0400

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = {}
+        def dfs(i, j):
+            if i == m - 1 and j == n - 1: return 1
+            if i >= m or j >= n: return 0
+            if (i, j) in cache: return cache[(i, j)]
+            cache[(i, j)] = dfs(i + 1, j) + dfs(i, j + 1)
+            return cache[(i, j)]
+        return dfs(0, 0)
+            
