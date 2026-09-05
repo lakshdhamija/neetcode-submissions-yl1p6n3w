5073339ed628ab524ef6e867879d40c93dc32b44
Laksh Dhamija <laksh.dhamija@gmail.com>
2026-09-05 16:30:10 -0400

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < len(s) and not s[l].isalnum(): l += 1
+            while r > l and not s[r].isalnum(): r -= 1
+            if l < len(s) and r > l and s[l].lower() != s[r].lower(): return False
+            l += 1
+            r -= 1
+        return True
