bf4c956c7b3d672496b908dcd2c9e18ea8305da9
Laksh Dhamija <laksh.dhamija@gmail.com>
2026-09-19 22:32:39 -0400

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        cache = {}
+        def dfs(i, m):
+            if i == len(nums): return 0 if m == 0 else float('inf')
+            if m == 0: return float('inf')
+            if (i, m) in cache: return cache[(i, m)]
+            res, curSum = float('inf'), 0
+            for j in range(i, len(nums) - m + 1):
+                curSum += nums[j]
+                res = min(res, max(curSum, dfs(j + 1, m - 1)))
+            cache[(i, m)] = res
+            return cache[(i, m)]
+        return dfs(0, k)
